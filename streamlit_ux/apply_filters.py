c22ba56540875a634ad9a698a76fb5872f4a1da9
KevinBaRoss <kevin_bhayakaporn@berkeley.edu>
2022-02-21 09:28:19 -0800

    added code to filters

--- streamlit_ux/apply_filters.py
+++ streamlit_ux/apply_filters.py
@@ -17,3 +17,156 @@
 
 # Next:
 ### To Do: Kevin/Haoyu: Present the filtered dataframes/series to the Charts in page1 and page2
+
+
+import pandas as pd
+from pathlib import Path
+import matplotlib as plt
+import numpy as np
+import sqlalchemy
+import yfinance as yf
+import yahoo_fin.stock_info as si
+import os
+import streamlit_ux
+
+# Using the Pandas read_csv funcion and the Path module, 
+# read "sp500_sustainability_scores.csv" file into a Pandas DataFrame
+esg_df = pd.read_csv(
+    Path("sp500_sustainability_scores.csv")
+)
+
+# Review the DataFrame
+## esg_df.head()
+
+# Create the connection string for your SQLite database
+database_connection_string = 'sqlite:///'
+
+# Pass the connection string to the SQLAlchemy create_engine function
+engine = sqlalchemy.create_engine(database_connection_string)
+
+# Confirm that the database engine was created.
+engine
+
+esg_df.to_sql(
+    'esg_score_info', #New table name
+    engine, 
+    index=False, 
+    if_exists='replace'
+)
+
+# Confirm that the table was created by calling the table_names function
+## engine.table_names()
+
+
+
+e_score = e_options
+s_score = s_options 
+g_score = g_options
+
+# Create and execute a query to return esg data for tickers that match the chosen criteria.
+query1 = """
+SELECT company_ticker, environmentScore, socialScore, governanceScore
+FROM esg_score_info
+WHERE environmentScore >= """+str(e_score)+""" AND socialScore >= """+str(s_score)+""" AND governanceScore >= """+str(g_score)+""";
+"""
+
+# read in your SQL query results using pandas
+esg_df = pd.read_sql(query1, con=engine)
+
+# return your first five rows
+## esg_df.head()
+
+esg_ticker = esg_df['company_ticker']
+
+#Download historical data for S&P 500
+sp500_hist_data = yf.download(list(esg_ticker), start="2020-01-01", end="2020-04-30")
+
+#Count the number of column fetched
+column_count = (len(sp500_hist_data.columns))/6
+
+#Fetch only the "Adjusted Closing Price" from the data frame
+sp500_hist_data_closing = sp500_hist_data.iloc[:,0:int(column_count)]
+
+#Print the data frame to make sure the data is fetched correctly
+## sp500_hist_data_closing
+
+#Create the "Custom_Index" column to calculate the average price of all the filtered stock
+sp500_hist_data_closing['custom_index'] = sp500_hist_data_closing.mean(axis=1)
+
+#Select only the average price of all fetch stocks
+position_of_avg_price = len(sp500_hist_data_closing.columns)
+
+#Select only date and price
+sp500_hist_data_closing_only = sp500_hist_data_closing.iloc[:,int(position_of_avg_price)-1]
+
+#Print the data frame to make sure the data is fetched correctly
+## sp500_hist_data_closing_only
+
+#Find the division factor to transform the data to base 100 for easy comparison
+division_factor_filtered_sp500 = sp500_hist_data_closing_only.iloc[0]
+
+#Transform closing price into base 100 for easy comparison
+sp500_hist_data_closing_only = (sp500_hist_data_closing_only/division_factor_filtered_sp500)*100
+
+#Print the data frame to make sure the data is manipulated correctly
+## sp500_hist_data_closing_only.head()
+
+# Fetch the data for ALL S&P 500 as an index for comparison
+
+# Create and execute a query to return the list of tickers for S&P 500
+query_all_sp500 = """
+SELECT company_ticker
+FROM esg_score_info
+"""
+
+# read in your SQL query results using pandas
+all_sp500_df = pd.read_sql(query_all_sp500, con=engine)
+all_sp500_ticker = all_sp500_df['company_ticker']
+
+#Download historical data for S&P 500
+all_sp500_hist_data = yf.download(list(all_sp500_ticker), start="2020-01-01", end="2020-04-30")
+
+#Count the number of column fetched
+all_sp500_column_count = (len(all_sp500_hist_data.columns))/6
+
+#Fetch only the "Adjusted Closing Price" from the data frame
+all_sp500_hist_data_closing = all_sp500_hist_data.iloc[:,0:int(all_sp500_column_count)]
+
+#Print the data frame to make sure the data is fetched correctly
+## all_sp500_hist_data_closing.head()
+
+#Create the "Custom_Index" column to calculate the average price of ALL stock in S&P500
+all_sp500_hist_data_closing['custom_index'] = all_sp500_hist_data_closing.mean(axis=1)
+
+#Select only the average price of all fetch stocks
+all_position_of_avg_price = len(all_sp500_hist_data_closing.columns)
+
+#Select only date and price
+all_sp500_hist_data_closing_only = all_sp500_hist_data_closing.iloc[:,int(all_position_of_avg_price)-1]
+
+#Find the division factor to transform the data to base 100 for easy comparison
+division_factor_all_sp500 = all_sp500_hist_data_closing_only.iloc[0]
+
+#Transform closing price into base 100 for easy comparison
+all_sp500_hist_data_closing_only = (all_sp500_hist_data_closing_only/division_factor_all_sp500)*100
+
+#Print the data frame to make sure the data is manipulated correctly
+## all_sp500_hist_data_closing_only.head()
+
+#Plot the filtered data frame
+sp500_hist_data_closing_only.plot(
+    title="The Price Index of Stock Filtered", 
+    xlabel="Date", 
+    ylabel="Price Index", 
+    figsize=(10,7),
+    legend="top",
+    label="Filtered Result",
+    linewidth=3
+)
+
+#Plot the "ALL" S&P500 index data frame
+all_sp500_hist_data_closing_only.plot(
+    legend="top",
+    label="All S&P500 Benchmark",
+    linewidth=3
+)
